94560c67baf73f1b1af74abf31bd07bba98258c8
U-KMJP\e11655 <e11655@PN1IKSA26949A.kmjp.local>
2013-10-25 18:50:07 +0900

    done implementing button label changing method. added TODO list.

--- draw.js
+++ draw.js
@@ -1,6 +1,50 @@
-function alert(){
-alert("•`‚¢‚½‚à‚Ì‚ðÁ‹Ž‚µ‚Ü‚·")
+function printMessage(){
+    alert("•`‚¢‚½‚à‚Ì‚ðÁ‹Ž‚µ‚Ü‚·")
 }
+
+var label1="change label";
+var label2="other label";
+
+
+function changeLabel() {
+    if (document.getElementById("change").value != label2) {
+        document.getElementById("change").value = label2;
+    } else {
+        document.getElementById("change").value = label1;
+    }
+}
+
+// TODO: create new button. when the button is clicked,  draw rectangle into canvas
+// e.g. (10, 10) , (50, 50)
+function drawRectangle() {
+}
+
+// TODO: create new button. when the button is clicked,  draw lines into canvas
+// e.g. (10, 10) -> (10, 50)
+function drawLines() {
+}
+
+// TODO: create new button. when the button is clicked,  draw splines into canvas
+// e.g. (10, 10)  (10, 11) (10, 12) (15, 15) ....
+function drawSplines() {
+}
+
+// TODO: create new button. when the button is clicked, clear what is drawn on the canvas.
+function clearCanvas() {
+}
+
+// TODO: when mouse is clicked, get mouse position and show it on the page.
+function printMousePosition() {
+}
+
+// TODO: in order to paint, figure out what event to process 
+
+// TODO: implement paint method
+function paint() {
+}
+
+// TODO: refactor
+
 
 var cw = 400;
 var ch = 300;
