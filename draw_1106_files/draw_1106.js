e3886d2e8a4612a56d77f64751d45d6b757bbd08
takekazu <kazuma.takeuchi@konicaminolta.com>
2013-11-06 17:49:10 +0900

    mouse point

---
+++ draw_1106_files/draw_1106.js
@@ -0,0 +1,66 @@
+var label1="change label";
+var label2="other label";
+
+function printMessage(){
+    alert("warning")
+}
+
+function changeLabel() {
+    if (document.getElementById("change").value != label2) {
+        document.getElementById("change").value = label2;
+    } else {
+        document.getElementById("change").value = label1;
+    }
+}
+
+// TODO: create new button. when the button is clicked,  draw rectangle into canvas
+// e.g. (10, 10) , (50, 50)
+function drawRectangle() {
+	var canvas = document.getElementById("canvas");
+	var context = canvas.getContext("2d");
+    context.fillRect(10, 10, 50, 50);
+}
+
+// TODO: create new button. when the button is clicked,  draw lines into canvas
+// e.g. (100, 100) -> (150, 150)
+function drawLines() {
+    var canvas = document.getElementById("canvas");
+    var context = canvas.getContext("2d");
+    context.beginPath();
+    context.moveTo(100, 100);
+    context.lineTo(200, 100);
+    context.closePath();
+    context.stroke();
+}
+
+// TODO: create new button. when the button is clicked,  draw splines into canvas
+// e.g. (100, 100)  (100, 110) (100, 120) (150, 150) ....
+function drawSplines() {
+    var canvas = document.getElementById("canvas");
+    var context = canvas.getContext("2d");
+    context.beginPath();
+    context.moveTo(100, 100);
+    context.quadraticCurveTo(50, 90, 100, 110);
+    context.quadraticCurveTo(40, 10, 100, 120);
+    context.quadraticCurveTo(20, 80, 150, 150);
+    context.stroke();
+}
+
+// TODO: create new button. when the button is clicked, clear what is drawn on the canvas.
+function clearCanvas() {
+    var canvas = document.getElementById("canvas");
+    var context = canvas.getContext("2d");
+    context.clearRect(0, 0, canvas.height, canvas.width);
+}
+
+// TODO: when mouse is clicked, get mouse position and show it on the page.
+function printMousePosition() {
+}
+
+// TODO: in order to paint, figure out what event to process 
+
+// TODO: implement paint method
+function paint() {
+}
+
+// TODO: refactor
